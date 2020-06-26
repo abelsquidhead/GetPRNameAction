ac3a009bbf1531704820d3c663391db51b07fa95
Abel Wang <abel.wang@microsoft.com>
2020-06-25 17:15:13 -0700

    initial checkin, getting values and storing them as env variables

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // get input values
+    const myToken = core.getInput('myToken');
+    console.log("myToken: " + myToken);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // get reference to octokit
+    const octokit = github.getOctokit(myToken);
 
-    core.setOutput('time', new Date().toTimeString())
+    // get reference to current context and get info about pull request
+    const context = github.context;
+    const prUrl = context.payload.pull_request?.html_url;
+    const prBody = context.payload.pull_request?.body;
+    const prNumber: number = <number>context.payload.pull_request?.number;
+    const { data: pullRequest } = await octokit.pulls.get( {
+      owner: 'abelsquidhead',
+      repo: 'TailwindsTraders-Website',
+      pull_number: prNumber,
+ 
+    });
+    const title = pullRequest.title
+
+    // print out the info from PR
+    console.log("prUrl: " + prUrl);
+    console.log("prBody: " + prBody);
+    console.log("title: " + title);
+
+
   } catch (error) {
     core.setFailed(error.message)
   }
